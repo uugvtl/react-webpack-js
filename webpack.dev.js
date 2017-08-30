385f71e9fd4466154cd55df9f34b9003e0a30896
Leon <uugv_tl@163.com>
2017-08-30 14:52:33 +0800

    加入babel-polyfill库

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,7 +15,7 @@ module.exports = {
         extensions: ['.js', '.jsx', '.json']
     },
     entry: {
-        app:path.join(__dirname, 'src', 'index.js')
+        app:['babel-polyfill',path.join(__dirname, 'src', 'index.js')]
     },
     output: {
         filename: 'assets/[name].js',
