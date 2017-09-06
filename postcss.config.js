7267e21ac7c0783d652efdb72c544f43fc6601fe
Leon <uugv_tl@163.com>
2017-09-06 17:46:20 +0800

    加入eslint配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: {
         'postcss-import': {},
-        'postcss-cssnext': {},
         'cssnano': {},
         'autoprefixer': {browsers: 'last 5 version'}
     }
